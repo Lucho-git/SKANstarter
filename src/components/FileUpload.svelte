e6516f2825ad6acd50f6a0c2a4a745234cadd090
Lachlan Ross <lachlan.f.ross@gmail.com>
2024-03-26 21:42:12 +0800

    Fixed file upload button colour problem

--- src/components/FileUpload.svelte
+++ src/components/FileUpload.svelte
@@ -63,8 +63,7 @@
 <div class="my-2">
   <input
     type="file"
-    class="file-input file-input-bordered file-input-accent w-full max-w-xs"
-    style="color: #000;"
+    class="file-input file-input-bordered file-input-primary text-primary w-full max-w-xs"
     on:change={handleFileChange}
   />
 </div>
