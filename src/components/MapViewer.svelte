b435cdfa2a29358efa9de22b9ba616a8ddbc2415
Lachlan Ross <lachlan.f.ross@gmail.com>
2024-05-28 14:59:57 +0800

    removed some logging

--- src/components/MapViewer.svelte
+++ src/components/MapViewer.svelte
@@ -167,7 +167,6 @@
       const vehicleIcon = userMarker.getElement().querySelector(".vehicle-icon")
       if (vehicleIcon) {
         vehicleIcon.style.transform = `rotate(${currentRotation}deg)`
-        console.log("Rotated vehicle icon to", currentRotation)
       }
 
       currentStep++
