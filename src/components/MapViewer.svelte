48d9473eeae367bb4e70761cfe401ae3a6d748ef
Lachlan Ross <lachlan.f.ross@gmail.com>
2024-05-28 19:35:40 +0800

    Moved vehicle tracking logic into vehicleTracker

--- src/components/MapViewer.svelte
+++ src/components/MapViewer.svelte
@@ -2,14 +2,13 @@
   import { onMount, onDestroy, setContext } from "svelte"
   import mapboxgl from "mapbox-gl"
   import "mapbox-gl/dist/mapbox-gl.css"
-  import { debounce } from "lodash-es"
   import { mapStore, userVehicleStore } from "../stores/mapStore"
   import MarkerManager from "./MarkerManager.svelte"
 
-  import UserMarker from "./UserMarker.svelte"
   import ButtonSection from "./ButtonSection.svelte"
   import MapControls from "./MapControls.svelte"
   import MapStateSaver from "./MapStateSaver.svelte"
+  import VehicleTracker from "./VehicleTracker.svelte" // Add this import
 
   //Constants and variable initializations
 
@@ -47,36 +46,6 @@
     mapOptions.container = mapContainer
     map = new mapboxgl.Map(mapOptions)
 
-    // Add the GeolocateControl to the map
-    const geolocateControl = new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true,
-      },
-      trackUserLocation: true,
-      showUserHeading: true,
-      showAccuracyCircle: true,
-      showUserLocation: false,
-    })
-
-    map.addControl(geolocateControl, "bottom-right")
-    // Trigger the geolocate action when the map loads
-    map.on("load", () => {
-      geolocateControl.trigger()
-    })
-
-    // Create a custom user location marker
-    userMarker = new mapboxgl.Marker({
-      element: createUserMarkerElement(),
-      pitchAlignment: "map",
-      rotationAlignment: "map",
-    })
-
-    // Update the user location marker on geolocate event
-    geolocateControl.on("geolocate", (e) => {
-      const { coords } = e
-      updateMarkerPosition(coords)
-    })
-
     mapStore.set(map)
     mapInitialized = true
   })
@@ -108,78 +77,6 @@
     markerClickEvent = event.detail
   }
 
-  function createUserMarkerElement() {
-    const el = document.createElement("div")
-
-    new UserMarker({
-      target: el,
-      props: {
-        pulseColor: "rgba(172, 172, 230, 0.8)",
-        pulseSize: "40px",
-        vehicleSize: $userVehicleStore.size,
-        userVehicle: $userVehicleStore.type,
-        vehicleColor: $userVehicleStore.color,
-      },
-    })
-
-    return el
-  }
-
-  let currentRotation = 0
-  let currentLat = 0
-  let currentLng = 0
-
-  const updateMarkerPosition = debounce((coords) => {
-    const { latitude, longitude, heading } = coords
-
-    const targetLat = latitude
-    const targetLng = longitude
-    const targetRotation = heading
-
-    const latDiff = targetLat - currentLat
-    const lngDiff = targetLng - currentLng
-    const rotationDiff = targetRotation - currentRotation
-
-    const distanceThreshold = 0.00001 // Adjust this value as needed
-    const distance = Math.sqrt(latDiff ** 2 + lngDiff ** 2)
-
-    if (distance < distanceThreshold) {
-      // If the distance is too small, skip the animation
-      return
-    }
-
-    const duration = ANIMATION_DURATION // Duration in milliseconds
-    const steps = duration / 16 // Number of steps based on 60 FPS (16.67ms per frame)
-
-    const latStep = latDiff / steps
-    const lngStep = lngDiff / steps
-    const rotationStep = rotationDiff / steps
-
-    let currentStep = 0
-
-    function animateMarker() {
-      currentLat += latStep
-      currentLng += lngStep
-      currentRotation += rotationStep
-
-      userMarker.setLngLat([currentLng, currentLat])
-
-      const vehicleIcon = userMarker.getElement().querySelector(".vehicle-icon")
-      if (vehicleIcon) {
-        vehicleIcon.style.transform = `rotate(${currentRotation}deg)`
-      }
-
-      currentStep++
-
-      if (currentStep < steps) {
-        requestAnimationFrame(animateMarker)
-      }
-    }
-
-    userMarker.setLngLat([currentLng, currentLat]).addTo(map)
-    animateMarker()
-  }, ANIMATION_DURATION)
-
   function toggleMapStyle() {
     if (isSatelliteStyle) {
       map.setStyle(DEFAULT_OUTDOORS_STYLE)
@@ -201,6 +98,7 @@
       on:markerPlacement={handleMarkerPlacement}
       on:markerClick={handleMarkerClick}
     />
+    <VehicleTracker {map} />
   {/if}
 </div>
 
--- src/components/VehicleTracker.svelte
+++ src/components/VehicleTracker.svelte
@@ -4,14 +4,19 @@
   import mapboxgl from "mapbox-gl"
   import { userVehicleStore } from "../stores/mapStore"
   import UserMarker from "./UserMarker.svelte"
+  import { debounce } from "lodash-es"
 
   export let map
 
   let geolocateControl
   let userMarker
 
+  const ANIMATION_DURATION = 500 // Adjust this value as needed
+  const DISTANCE_THRESHOLD = 0.00001
+
   onMount(() => {
     // Create the geolocateControl and add it to the map
+    console.log("Adding geolocateControl to the map")
     geolocateControl = new mapboxgl.GeolocateControl({
       positionOptions: {
         enableHighAccuracy: true,
@@ -22,7 +27,9 @@
       showUserLocation: false,
     })
     map.addControl(geolocateControl, "bottom-right")
-
+    map.on("load", () => {
+      geolocateControl.trigger()
+    })
     // Create the userMarker
     userMarker = new mapboxgl.Marker({
       element: createUserMarkerElement(),
@@ -39,8 +46,9 @@
 
   onDestroy(() => {
     // Clean up the geolocateControl and userMarker
-    geolocateControl.remove()
-    userMarker.remove()
+    if (userMarker) {
+      userMarker.remove()
+    }
   })
 
   function createUserMarkerElement() {
@@ -60,8 +68,58 @@
     return el
   }
 
-  function updateMarkerPosition(coords) {
-    const { latitude, longitude } = coords
-    userMarker.setLngLat([longitude, latitude]).addTo(map)
-  }
+  let currentRotation = 0
+  let currentLat = 0
+  let currentLng = 0
+
+  const updateMarkerPosition = debounce((coords) => {
+    const { latitude, longitude, heading } = coords
+
+    const targetLat = latitude
+    const targetLng = longitude
+    const targetRotation = heading
+
+    const latDiff = targetLat - currentLat
+    const lngDiff = targetLng - currentLng
+    const rotationDiff = targetRotation - currentRotation
+
+    const distanceThreshold = DISTANCE_THRESHOLD // Adjust this value as needed
+    const distance = Math.sqrt(latDiff ** 2 + lngDiff ** 2)
+
+    if (distance < distanceThreshold) {
+      // If the distance is too small, skip the animation
+      return
+    }
+
+    const duration = ANIMATION_DURATION // Duration in milliseconds
+    const steps = duration / 16 // Number of steps based on 60 FPS (16.67ms per frame)
+
+    const latStep = latDiff / steps
+    const lngStep = lngDiff / steps
+    const rotationStep = rotationDiff / steps
+
+    let currentStep = 0
+
+    function animateMarker() {
+      currentLat += latStep
+      currentLng += lngStep
+      currentRotation += rotationStep
+
+      userMarker.setLngLat([currentLng, currentLat])
+
+      const vehicleIcon = userMarker.getElement().querySelector(".vehicle-icon")
+      if (vehicleIcon) {
+        vehicleIcon.style.transform = `rotate(${currentRotation}deg)`
+      }
+
+      currentStep++
+
+      if (currentStep < steps) {
+        requestAnimationFrame(animateMarker)
+      }
+    }
+
+    userMarker.setLngLat([currentLng, currentLat]).addTo(map)
+    animateMarker()
+  }, ANIMATION_DURATION)
 </script>
