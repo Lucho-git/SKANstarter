53512f6ad1302be1ae65656a10b50adb3a75e1c7
Lachlan Ross <lachlan.f.ross@gmail.com>
2024-06-01 17:13:28 +0800

    cookie options

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -9,11 +9,17 @@ import { createClient } from "@supabase/supabase-js"
 import type { Handle } from "@sveltejs/kit"
 
 export const handle: Handle = async ({ event, resolve }) => {
-  event.locals.supabase = createSupabaseServerClient({
-    supabaseUrl: PUBLIC_SUPABASE_URL,
-    supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
-    event,
-  })
+    event.locals.supabase = createSupabaseServerClient({
+      supabaseUrl: PUBLIC_SUPABASE_URL,
+      supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+      event,
+      cookieOptions: {
+        domain: ".supabase.co", // Set the cookie domain to ".supabase.co"
+        secure: true, // Set the secure flag to true since Supabase uses HTTPS
+        sameSite: "lax", // Adjust the sameSite option based on your requirements
+        path: "/", // Set the cookie path to "/"
+      },
+    })
 
   event.locals.supabaseServiceRole = createClient(
     PUBLIC_SUPABASE_URL,
