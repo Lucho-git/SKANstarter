8c1d42221bd2f0f11a90899da9a05c27e9fff685
Lachlan Ross <lachlan.f.ross@gmail.com>
2024-07-19 17:45:28 +0800

    Shorthash version

--- version.js
+++ version.js
@@ -6,13 +6,10 @@ const BASE_VERSION = 1;
 // Get the number of commits in the current branch
 const commitCount = parseInt(execSync('git rev-list --count HEAD').toString().trim());
 
-// Get the latest commit hash
-const gitHash = execSync('git rev-parse HEAD').toString().trim();
-
-// Convert the hash to a numeric value
-const numericHash = parseInt(gitHash.substring(0, 8), 16);
+// Get the latest short commit hash
+const gitShortHash = execSync('git rev-parse --short HEAD').toString().trim();
 
 // Combine all parts
-const version = `${BASE_VERSION}.${commitCount}.${numericHash % 10000}`;
+const version = `${BASE_VERSION}.${commitCount}.${gitShortHash}`;
 
 console.log(`PUBLIC_APP_VERSION=${version}`);
