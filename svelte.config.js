93a7445b3d681ccf045d3273ee6ce35dadb5957e
Lachlan Ross <lachlan.f.ross@gmail.com>
2024-07-19 15:07:42 +0800

    Added versioning, and reload on new version features

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
-// import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import * as child_process from 'node:child_process';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
         adapter: adapter({
             runtime: 'nodejs18.x'
-        })
+        }),
+        version: {
+            name: child_process.execSync('git rev-parse HEAD').toString().trim(),
+            pollInterval: 60000 // Check for updates every minute
+        }
     },
     preprocess: vitePreprocess()
 };
