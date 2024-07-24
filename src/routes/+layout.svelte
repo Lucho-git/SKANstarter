5d8a0649f812528eb2bb489c3b66c8e59812c400
Lachlan Ross <lachlan.f.ross@gmail.com>
2024-07-24 21:57:29 +0800

    Longer refresh hold

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,7 +26,7 @@
         label: "Refresh",
         onClick: () => location.reload(),
       },
-      duration: 10000, // 10 seconds in milliseconds
+      duration: 100000, // 10 seconds in milliseconds
     })
   }
 </script>
