10ad5a25a968bde919f1ba546c4c1efc06e3f0e3
Lachlan Ross <lachlan.f.ross@gmail.com>
2024-07-30 03:08:14 +0800

    Notification enabler popup

---
+++ src/components/NotificationsBanner.svelte
@@ -0,0 +1,65 @@
+<script>
+  import { fly } from "svelte/transition"
+  import { subscribeToPushNotifications } from "$lib/pushNotifications"
+
+  let showBanner = true
+
+  function closeBanner() {
+    showBanner = false
+  }
+
+  async function enableNotifications() {
+    await subscribeToPushNotifications(/* userId */)
+    showBanner = false
+  }
+</script>
+
+{#if showBanner}
+  <div
+    class="fixed top-0 left-0 right-0 z-50 flex justify-center"
+    transition:fly={{ y: -100, duration: 500 }}
+  >
+    <div class="alert shadow-lg m-2 sm:w-full md:w-2/3 lg:w-1/3">
+      <div class="flex-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="w-6 h-6 mx-2 stroke-current"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          ></path>
+        </svg>
+        <span
+          >We'd like to show you notifications to keep you up to date with
+          development and changes to your farm</span
+        >
+      </div>
+      <div class="flex-none">
+        <button class="btn btn-sm btn-primary" on:click={enableNotifications}>
+          Get notifications
+        </button>
+        <button class="btn btn-sm btn-ghost" on:click={closeBanner}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+{/if}
--- src/routes/(admin)/account/+layout.svelte
+++ src/routes/(admin)/account/+layout.svelte
@@ -4,15 +4,57 @@
   import { page } from "$app/stores"
   import { browser } from "$app/environment"
   import { subscribeToPushNotifications } from "$lib/pushNotifications"
+  import { fly } from "svelte/transition"
+  import { toast } from "svelte-sonner"
 
   export let data
 
   let { supabase, session } = data
   $: ({ supabase, session } = data)
 
+  const TWO_DAYS_MS = 24 * 60 * 60 * 1000
+
+  let showNotificationBanner = false
+
+  function closeBanner() {
+    showNotificationBanner = false
+    localStorage.setItem(
+      "notificationBannerInteraction",
+      new Date().getTime().toString(),
+    )
+  }
+
+  async function requestPushNotification() {
+    if (session && session.user) {
+      toast.promise(subscribeToPushNotifications(session.user.id), {
+        loading: "Enabling notifications...",
+        success: (result) => {
+          if (result.success) {
+            showNotificationBanner = false
+            localStorage.setItem(
+              "notificationBannerInteraction",
+              new Date().getTime().toString(),
+            )
+            return "Notifications enabled successfully!"
+          } else {
+            throw new Error("Failed to enable notifications")
+          }
+        },
+        error: (error) => `Error: ${error.message}`,
+      })
+    }
+  }
+
   function checkAndSubscribe(userId: string) {
     if (browser) {
       console.log("Running in browser environment")
+
+      // Check if notifications are already enabled
+      if (Notification.permission === "granted") {
+        console.log("Push notifications are already enabled")
+        return
+      }
+
       const isStandalone = window.matchMedia(
         "(display-mode: standalone)",
       ).matches
@@ -24,15 +66,32 @@
       const lastCheckedUserId = localStorage.getItem(
         "lastCheckedPushNotificationsUserId",
       )
-      console.log("Last checked user ID:", lastCheckedUserId)
+      const lastCheckedTime = localStorage.getItem(
+        "lastCheckedPushNotificationsTime",
+      )
+      const currentTime = new Date().getTime()
 
-      if (isStandalone && isHomepage && lastCheckedUserId !== userId) {
-        console.log("Conditions met, subscribing to push notifications")
-        subscribeToPushNotifications(userId)
+      console.log("Last checked user ID:", lastCheckedUserId)
+      console.log("Last checked time:", lastCheckedTime)
+
+      const shouldCheck =
+        lastCheckedUserId !== userId ||
+        !lastCheckedTime ||
+        (lastCheckedTime &&
+          currentTime - parseInt(lastCheckedTime) > TWO_DAYS_MS)
+
+      if (isStandalone && isHomepage && shouldCheck) {
+        console.log("Conditions met for showing notification banner")
+        setTimeout(() => {
+          showNotificationBanner = true
+        }, 2000)
         localStorage.setItem("lastCheckedPushNotificationsUserId", userId)
-        console.log("Push notification check completed for user:", userId)
+        localStorage.setItem(
+          "lastCheckedPushNotificationsTime",
+          currentTime.toString(),
+        )
       } else {
-        console.log("Conditions not met for push notification subscription")
+        console.log("Conditions not met for showing notification banner")
       }
     } else {
       console.log("Not running in browser environment")
@@ -45,8 +104,9 @@
     const { data } = supabase.auth.onAuthStateChange((event, _session) => {
       console.log("Auth state changed:", event)
       if (event === "SIGNED_OUT") {
-        console.log("User signed out, clearing last checked user ID")
+        console.log("User signed out, clearing last checked data")
         localStorage.removeItem("lastCheckedPushNotificationsUserId")
+        localStorage.removeItem("lastCheckedPushNotificationsTime")
       } else if (_session && _session.user) {
         console.log("User signed in, checking push notifications")
         checkAndSubscribe(_session.user.id)
@@ -67,5 +127,51 @@
     }
   })
 </script>
+
+{#if showNotificationBanner}
+  <div
+    class="fixed top-2 left-0 right-0 z-50 flex justify-center"
+    transition:fly={{ y: -100, duration: 500 }}
+  >
+    <div
+      class="w-full md:w-2/3 lg:w-1/3 bg-secondary text-secondary-content p-6 rounded-lg shadow-lg mx-4 md:mx-0"
+    >
+      <div class="flex items-start mb-6">
+        <div class="flex-shrink-0 mr-6">
+          <i class="at-bell-check text-4xl p-2"></i>
+        </div>
+        <p class="text-sm md:text-base flex-grow pr-4">
+          We'd like to show you notifications for the latest developments to
+          your farm
+        </p>
+        <button
+          class="text-secondary-content hover:text-primary ml-4 flex-shrink-0"
+          on:click={closeBanner}
+        >
+          <svg
+            class="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <button
+        class="btn btn-primary w-full py-3 px-6"
+        on:click={requestPushNotification}
+      >
+        Get notifications
+      </button>
+    </div>
+  </div>
+{/if}
 
 <slot />
