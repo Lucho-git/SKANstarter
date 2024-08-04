e0d3a6777a3d63e4afa64d7466983d17151f8220
Lachlan Ross <lachlan.f.ross@gmail.com>
2024-08-05 01:34:09 +0800

    Clears auth token cache on signout

--- src/routes/(admin)/account/(menu)/+page.server.ts
+++ src/routes/(admin)/account/(menu)/+page.server.ts
@@ -1,11 +1,21 @@
 import { redirect } from "@sveltejs/kit"
 
 export const actions = {
-  signout: async ({ locals: { supabase, getSession } }) => {
-    const session = await getSession()
-    if (session) {
-      await supabase.auth.signOut()
-      throw redirect(303, "/")
-    }
-  },
+    signout: async ({ locals: { supabase, getSession } }) => {
+        const session = await getSession()
+        try {
+          await supabase.auth.signOut()
+        } catch (error) {
+          console.error("Error during sign-out:", error)
+        } finally {
+          // Clear any local storage items related to the session
+          if (typeof window !== 'undefined') {
+            console.log('Clearing local storage session...')
+            localStorage.removeItem('supabase.auth.token')
+            // Add any other items that need to be cleared
+          }
+          throw redirect(303, "/")
+        }
+      }
+      
 }
