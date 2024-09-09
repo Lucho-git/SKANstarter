58291e371ea2c017f8bf12f252080374e4b224a8
Lachlan Ross <lachlan.f.ross@gmail.com>
2024-09-09 13:41:30 +0800

    Fixed error page, by making the page store only accessible in the browser

--- src/stores/crispVisibilityStore.ts
+++ src/stores/crispVisibilityStore.ts
@@ -1,29 +1,34 @@
-// stores/crispVisibilityStore.ts
-import { writable } from 'svelte/store';
-
-import { derived } from 'svelte/store';
+import { browser } from '$app/environment';
+import { derived, writable } from 'svelte/store';
 import { page } from '$app/stores';
 import { screenSize } from './screenSizeStore';
 
 export const mobileChat = writable(false);
 
+function createCrispVisibilityStore() {
+    if (browser) {
+        return derived(
+            [page, screenSize, mobileChat],
+            ([$page, $screenSize, $mobileChat]) => {
+                const hiddenPaths = ['/account/settings', '/account/mapviewer'];
 
-export const crispVisibility = derived(
-    [page, screenSize, mobileChat],
-    ([$page, $screenSize, $mobileChat]) => {
-        const hiddenPaths = ['/account/settings', '/account/mapviewer'];
-
-        if ($screenSize === 'lg') {
-            return !hiddenPaths.some(path => $page.url?.pathname?.startsWith(path) ?? false);
-        } else {
-            return $mobileChat;
-        }
+                if ($screenSize === 'lg') {
+                    return !hiddenPaths.some(path => $page.url?.pathname?.startsWith(path) ?? false);
+                } else {
+                    return $mobileChat;
+                }
+            }
+        );
     }
-);
 
+    // Return a dummy store for server-side rendering
+    return writable(false);
+}
 
-// Log changes
-crispVisibility.subscribe(value => {
-    console.log('Crisp visibility changed:', value);
-});
+export const crispVisibility = createCrispVisibilityStore();
 
+if (browser) {
+    crispVisibility.subscribe(value => {
+        console.log('Crisp visibility changed:', value);
+    });
+}
