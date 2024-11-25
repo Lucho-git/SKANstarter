31eeb5970aecec751f3fdd69e72a49ee8f74176f
Lachlan Ross <lachlan.f.ross@gmail.com>
2024-11-25 23:14:09 +0800

    Removed sync button

--- src/components/ButtonSection.svelte
+++ src/components/ButtonSection.svelte
@@ -226,7 +226,7 @@
         : 'h-50 scale-0 overflow-hidden opacity-0'}"
     >
       <!--Sync Button-->
-      <button
+      <!-- <button
         class="menu-button {currentStyle} btn {isCircular
           ? 'btn-circle'
           : 'btn-square'} btn-lg bg-white hover:bg-opacity-90"
@@ -246,7 +246,7 @@
             d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
           />
         </svg>
-      </button>
+      </button> -->
 
       <!-- Drawing Mode Toggle Button -->
       <button
