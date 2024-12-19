5a9fa3aa4ed21ec1169b4c45c3fe9a3604295944
Lachlan Ross <lachlan.f.ross@gmail.com>
2024-12-19 23:16:37 +0800

    fixed carousel bg color

--- src/routes/(new-landing)/new/PartnerSection.svelte
+++ src/routes/(new-landing)/new/PartnerSection.svelte
@@ -61,7 +61,8 @@
 </script>
 
 <div
-  class="relative flex h-32 w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-[#f5f3e8] md:shadow-xl"
+  class="relative flex h-32 w-full flex-col items-center justify-center overflow-hidden rounded-lg
+      border border-base-300 bg-base-300 md:shadow-xl"
 >
   <Marquee reverse pauseOnHover class="[--duration:20s]">
     {#each partners as partner}
@@ -76,9 +77,11 @@
     {/each}
   </Marquee>
   <div
-    class="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-[#f5f3e8]"
+    class="pointer-events-none absolute inset-y-0 left-0 w-1/3
+        bg-gradient-to-r from-base-200"
   ></div>
   <div
-    class="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-[#f5f3e8]"
+    class="pointer-events-none absolute inset-y-0 right-0 w-1/3
+        bg-gradient-to-l from-base-200"
   ></div>
 </div>
